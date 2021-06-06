26441a6699f49fabd22609af67f173f60790b56b
fernandoroj3345 <fernandorojas201482@gmail.com>
2021-06-06 19:17:22 -0300

    Borro archivos innecesarios

--- RECURSIVIDAD/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def Usarlafuerza(arr,x,indice):
-        if arr[indice] == x:
-            print(x)
-            print('se sacaron ',indice,'objetos')
-        else:
-            print(arr[indice])
-            Usarlafuerza(arr,x,indice+1)
-mochila = ['enano','ryzen9','piedra','sable de luz','papel']
-Usarlafuerza(mochila,'sable de luz',0)
