ef9b5c260e6e77f6a13ed74e193e56d3d094b2c6
Fernando <fernandorojas201482@gmail.com>
2021-11-20 23:54:54 -0300

    done

--- GRAFO/EJ6.py
+++ GRAFO/EJ6.py
@@ -3,40 +3,42 @@ from tda_grafo import Grafo
 20 de la guía de árboles (capítulo X), convertirlo en un grafo y resolver las siguientes actividades:
 A. además del nombre de los dioses, deberá cargar una breve descripción de quien es o lo que
 representa, no más de 20 palabras;
-b. deberá cargar todas las relaciones entre los distintos dioses: padre, madre, hijo, hermano,
+B. Deberá cargar todas las relaciones entre los distintos dioses: padre, madre, hijo, hermano,
 pareja, la etiquetas de dichas aristas son estos nombre de relación.
-C. dado el nombre de un dios mostrar los hijos de este;
-D. dado el nombre de un dios mostrar su nombre, padre, madre, hermanos y sus hijos;
-E. determinar si existe relación directa entre dos vértice cualquieras, de ser así cual es la relación
-entre ambos;
-F. dados dos dioses determinar el camino más corto entre estos y mostrarlo. Considere como
-camino más corto el que tenga menor número de aristas;
-F. realizar un barrido en profundidad y amplitud de dicho grafo;
-H. realizar un barrido mostrando el nombre de cada dios y el de su madre;
-I. mostrar todos los ancestros de un determinado dios;
-J. mostrar todos los nietos de Cronos;
-K. mostrar todos los hijos de Tea;
-L. persista los datos del grafo en archivos, uno para los vértices y otro para las aristas.'''
+C. Dado el nombre de un dios mostrar los hijos de este;
+D. Dado el nombre de un dios mostrar su nombre, padre, madre, hermanos y sus hijos;
+E. Determinar si existe relación directa entre dos vértice cualquieras, de ser así cual es la relación
+    entre ambos;
+F. Dados dos dioses determinar el camino más corto entre estos y mostrarlo. Considere como
+    camino más corto el que tenga menor número de aristas;
+F. Realizar un barrido en profundidad y amplitud de dicho grafo;
+H. Realizar un barrido mostrando el nombre de cada dios y el de su madre;
+I. Mostrar todos los ancestros de un determinado dios;
+J. Mostrar todos los nietos de Cronos;
+K. Mostrar todos los hijos de Tea;
+L. Persista los datos del grafo en archivos, uno para los vértices y otro para las aristas.'''
 
 DiosesGriegos= Grafo()
-
+#abro el archivo de texto y  los datos del archivo y cargo los datos.
 file = open('Dioses.txt', encoding="utf8")
 lineas = file.readlines()
 lineas.pop(0)
 for linea in lineas:
-    dios = linea.split(';')
-    dios_data = {}
+    dios = linea.split(';')#con split voy cortando los textos que que esten separados con ";" y los asocio a
+                                                                    #los datos que agrego al dicccionario. 
+    dios_data = {}#diccionario vacio.
     nombre = dios[0]
-    dios_data['descripcion'] = dios[5]
-    dios_data['genero'] = dios[6]
+    dios_data['descripcion'] = dios[5]#asigno el valor 5 que corte
+    dios_data['genero'] = dios[6]# "" "" "" "" "" "" "" "" "" "" ""
     DiosesGriegos.insertar_vertice(nombre, data = dios_data)
 
+#cargo las lineas
 for linea in lineas:
     dios = linea.split(';')
     nombre = dios[0]
     padre = dios[1]
     madre = dios[2]
-    hermanos = dios[3].split('/')#Devuelve solo un array en lugar de una linea de texto
+    hermanos = dios[3].split('/')#Devuelve solo un array en lugar de una linea de texto.
     hijos = dios[4].split('/')
     genero = dios[6]
     pareja = dios[7].split('\n')[0]
@@ -60,8 +62,9 @@ for linea in lineas:
 print('Ejercicio 06')
 print()
 
-#! ---- PUNTO C ----!#
-# c. dado el nombre de un dios mostrar los hijos de este;
+#C)
+#Dado el nombre de un dios mostrar los hijos de este;
+
 nombre_dios = input('Ingrese el nombre de un dios para ver a sus hijos: ').capitalize()
 hijos = DiosesGriegos.hijos_dios(nombre_dios)
 if (hijos == None):
@@ -74,8 +77,8 @@ else:
     print ('No hay informacion sobre los hijos de', nombre_dios)
 print()
 
-#! ---- PUNTO D ----!#
-# d. dado el nombre de un dios mostrar su nombre, padre, madre, hermanos y sus hijos;
+#Dado el nombre de un dios mostrar su nombre, padre, madre, hermanos y sus hijos;
+#D)
 nombre_dios = input('Ingrese el nombre de un dios para ver a su familia: ').capitalize()
 padres, hijos, hermanos = DiosesGriegos.familia_dios(nombre_dios)
 
@@ -98,9 +101,9 @@ else:
 print()
 
 
-#! ---- PUNTO E ----!#
-# e. determinar si existe relación directa entre dos vértice cualquieras, de ser así cual es la relación
-# entre ambos;
+#E)
+#Determinar si existe relación directa entre dos vértice cualquieras, de ser así cual es la relación
+#entre ambos;
 print('A continuacion, ingrese los nombres de dos DiosesGriegos para determinar si tienen una relacion directa.')
 vertice_origen = input('Ingrese el nombre del primer dios: ').capitalize()
 vertice_destino = input('Ingrese el nombre del segundo dios: ').capitalize()
@@ -113,9 +116,9 @@ else:
     print('No existe relacion directa entre', vertice_origen, 'y', vertice_destino)
 print()
 
-#! ---- PUNTO F ----!#
-# f. dados dos DiosesGriegos determinar el camino más corto entre estos y mostrarlo. Considere como
-# camino más corto el que tenga menor número de aristas;
+#F)
+#Dados dos DiosesGriegos determinar el camino más corto entre estos y mostrarlo. Considere como
+#camino más corto el que tenga menor número de aristas;
 
 print('A continuacion, ingrese los nombres de dos DiosesGriegos para determinar el camino mas corto entre ambos.')
 dios1 = input('Ingrese el nombre del primer dios: ').capitalize()
@@ -141,8 +144,8 @@ while(not pila_camino.pila_vacia()):
 print('El costo total del camino es', costo)
 print()
 
-#! ---- PUNTO G ----!#
-# g. realizar un barrido en profundidad y amplitud de dicho grafo;
+#G)
+#Realizar un barrido en profundidad y amplitud de dicho grafo;
 a = input('Presione enter para ver el barrido en profundidad.')
 DiosesGriegos.barrido_profundidad(0)
 print()
@@ -152,15 +155,15 @@ DiosesGriegos.barrido_amplitud(0)
 DiosesGriegos.marcar_no_visitado()
 print()
 
-#! ---- PUNTO H ----!#
-# h. realizar un barrido mostrando el nombre de cada dios y el de su madre;
+#H)
+#Realizar un barrido mostrando el nombre de cada dios y el de su madre;
 a = input('Presione enter para ver el barrido de los DiosesGriegos y sus madres.')
 DiosesGriegos.barrido_profundidad_dioses_madres(0)
 DiosesGriegos.marcar_no_visitado()
 print()
 
-#! ---- PUNTO I ----!#
-# i. mostrar todos los ancestros de un determinado dios;
+#I)
+#Mostrar todos los ancestros de un determinado dios;
 ancestros = []
 nombre_dios = input('Ingrese el nombre de un dios para ver a sus ancestros: ').capitalize()
 DiosesGriegos.ancestros_dios(nombre_dios, ancestros)
@@ -172,16 +175,16 @@ else:
     print ('No hay informacion sobre los ancestros de', nombre_dios)
 print()
 
-#! ---- PUNTO J ----!#
-# j. mostrar todos los nietos de Cronos;
+#J)
+#Mostrar todos los nietos de Cronos;
 print ('Nietos de Cronos:')
 nietos = DiosesGriegos.nietos_dios('Kronos')
 for nieto in nietos:
     print (' - ', nieto)
 print()
 
-#! ---- PUNTO K ----!#
-# k. mostrar todos los hijos de Tea;
+#K)
+#Mostrar todos los hijos de Tea;
 print ('Los hijos de Tea son:')
 hijos = DiosesGriegos.hijos_dios('Theia')
 for hijo in hijos:
--- GRAFO/Prueba_ej16.py
+++ GRAFO/Prueba_ej16.py
@@ -11,7 +11,7 @@ el templo de Apolo, en Delfos.'''
 
 templos = Grafo(dirigido=False)
 
-datos_templos = [{'nombre': 'Atenea (Partenon)', 'coordenadas' : {'latitud': '37°58′17″N', 'longitud': '23°43′36″E'}},
+datos_templos = [{'nombre': 'Atenas (Partenon)', 'coordenadas' : {'latitud': '37°58′17″N', 'longitud': '23°43′36″E'}},
                  {'nombre': 'Zeus (Olimpia)', 'coordenadas' : {'latitud': '37°38′16″N', 'longitud': '21°37′48″E'}},
                  {'nombre': 'Hera (Olimpia)', 'coordenadas' : {'latitud': '37°38′20″N', 'longitud': '21°37′47″E'}},
                  {'nombre': 'Apollo (Delfos)', 'coordenadas' : {'latitud': '38°28′56″N', 'longitud': '22°30′05″E'}},
@@ -25,30 +25,31 @@ for templo in datos_templos:
     templos.insertar_vertice(templo['nombre'], data = templo['coordenadas'])
 
 def cargar_aristas ():#Conexiones entre los nodos
-    templos.insertar_arista(303, 'Atenea (Partenon)', 'Zeus (Olimpia)')
-    templos.insertar_arista(303, 'Atenea (Partenon)', 'Hera (Olimpia)')
-    templos.insertar_arista(164, 'Atenea (Partenon)', 'Apollo (Delfos)')
-    templos.insertar_arista(899, 'Atenea (Partenon)', 'Poseidón (Sunión)')
-    templos.insertar_arista(482, 'Atenea (Partenon)', 'Artemisa (Éfeso)')
-    templos.insertar_arista(333, 'Atenea (Partenon)', 'Dionisio (Acrópolis)')
-    templos.insertar_arista(333, 'Zeus (Olimpia)', 'Hera (Olimpia)')
-    templos.insertar_arista(111, 'Zeus (Olimpia)', 'Apollo (Delfos)')
-    templos.insertar_arista(900, 'Zeus (Olimpia)', 'Poseidón (Sunión)')
-    templos.insertar_arista(890, 'Zeus (Olimpia)', 'Artemisa (Éfeso)')
-    templos.insertar_arista(111, 'Zeus (Olimpia)', 'Dionisio (Acrópolis)')
-    templos.insertar_arista(112, 'Hera (Olimpia)', 'Apollo (Delfos)')
-    templos.insertar_arista(312, 'Hera (Olimpia)', 'Poseidón (Sunión)')
-    templos.insertar_arista(321, 'Hera (Olimpia)', 'Artemisa (Éfeso)')
-    templos.insertar_arista(107, 'Hera (Olimpia)', 'Dionisio (Acrópolis)')
-    templos.insertar_arista(189, 'Apollo (Delfos)', 'Poseidón (Sunión)')
-    templos.insertar_arista(456, 'Apollo (Delfos)', 'Artemisa (Éfeso)')
-    templos.insertar_arista(212, 'Apollo (Delfos)', 'Dionisio (Acrópolis)')
-    templos.insertar_arista(567, 'Poseidón (Sunión)', 'Artemisa (Éfeso)')
+    templos.insertar_arista(127, 'Atenea (Partenon)', 'Zeus (Olimpia)')
+    templos.insertar_arista(987, 'Atenea (Partenon)', 'Hera (Olimpia)')
+    templos.insertar_arista(820, 'Atenea (Partenon)', 'Apollo (Delfos)')
+    templos.insertar_arista(712, 'Atenea (Partenon)', 'Poseidón (Sunión)')
+    templos.insertar_arista(632, 'Atenea (Partenon)', 'Artemisa (Éfeso)')
+    templos.insertar_arista(101, 'Atenea (Partenon)', 'Dionisio (Acrópolis)')
+    templos.insertar_arista(422, 'Zeus (Olimpia)', 'Hera (Olimpia)')
+    templos.insertar_arista(242, 'Zeus (Olimpia)', 'Apollo (Delfos)')
+    templos.insertar_arista(355, 'Zeus (Olimpia)', 'Poseidón (Sunión)')
+    templos.insertar_arista(769, 'Zeus (Olimpia)', 'Artemisa (Éfeso)')
+    templos.insertar_arista(12, 'Zeus (Olimpia)', 'Dionisio (Acrópolis)')
+    templos.insertar_arista(47, 'Hera (Olimpia)', 'Apollo (Delfos)')
+    templos.insertar_arista(69, 'Hera (Olimpia)', 'Poseidón (Sunión)')
+    templos.insertar_arista(10, 'Hera (Olimpia)', 'Artemisa (Éfeso)')
+    templos.insertar_arista(314, 'Hera (Olimpia)', 'Dionisio (Acrópolis)')
+    templos.insertar_arista(636, 'Apollo (Delfos)', 'Poseidón (Sunión)')
+    templos.insertar_arista(896, 'Apollo (Delfos)', 'Artemisa (Éfeso)')
+    templos.insertar_arista(111, 'Apollo (Delfos)', 'Dionisio (Acrópolis)')
+    templos.insertar_arista(111, 'Poseidón (Sunión)', 'Artemisa (Éfeso)')
     templos.insertar_arista(333, 'Poseidón (Sunión)', 'Dionisio (Acrópolis)')
-    templos.insertar_arista(339, 'Artemisa (Éfeso)', 'Dionisio (Acrópolis)')
+    templos.insertar_arista(444, 'Artemisa (Éfeso)', 'Dionisio (Acrópolis)')
 
 cargar_aristas()
 
+print('Ejercicio 16')
 print()
 
 #C)
@@ -56,7 +57,7 @@ print()
 #Expancion minima implica crear un bosque.
 bosque = templos.prim()
 peso = 0
-print('Arbol De Expansion Minimo:')
+print('Arbol de expansion minimo:')
 for elemento in bosque:
     print(' ', elemento[1][0], '-', elemento[1][1])
     peso += elemento[0] #Cuenta y acumula el peso
@@ -67,14 +68,14 @@ print()
 #Hallar el camino más corto para ir desde el templo de Atenea, el Partenón, en Atenas hasta
 #el templo de Apollo, en Delfos.
 
-n1 = 'Atenea (Partenon)'
-n2 = 'Apollo (Delfos)'
-ver_origen = templos.buscar_vertice(n1)
-ver_destino = templos.buscar_vertice(n2)
+nodo1 = 'Atenea (Partenon)'
+nodo2 = 'Apollo (Delfos)'
+ver_origen = templos.buscar_vertice(nodo1)
+ver_destino = templos.buscar_vertice(nodo2)
 
 pila_camino = templos.dijkstra(ver_origen, ver_destino)
 
-destino = n2
+destino = nodo2
 distancia = None
 
 print ('El camino mas corto es:')
@@ -86,4 +87,4 @@ while(not pila_camino.pila_vacia()):
         print(' - ', dato[1][0])#muestro el recorrido.
         destino = dato[1][1]
 
-print('La Distancia Total Del Camino Es De', distancia, 'Km.')
+print('La distancia total del camino es de', distancia, 'km.')
